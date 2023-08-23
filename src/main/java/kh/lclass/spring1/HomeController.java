ba69aedc6e2cfa433d6fb56f310d3afade60578c
Juni0505 <dudwns050505@gmail.com>
2023-08-23 14:03:18 +0900

    Spring 기본 설정
    porm.xml 버전 수정과 dependency추가

---
+++ src/main/java/kh/lclass/spring1/HomeController.java
@@ -0,0 +1,55 @@
+package kh.lclass.spring1;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.trace("trace");
+		logger.debug("debug");
+		logger.info("Welcome home! The client locale is {}.", locale);
+		// 앞으로 sysout은 정말정말 급하고 임의의 로그 볼떄.. 곧바로 지워질 메시지 찍을 때
+		logger.warn("warn");
+		logger.error("error");
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	@GetMapping("/aaa/a2")
+//	@RequestMapping(value = "/aaa/a2", method = RequestMethod.GET)
+	public void a2() {
+	}
+	@PostMapping("/aaa/a1")
+//	@RequestMapping(value = "/aaa/a1", method = RequestMethod.GET)
+	public String a1() {
+		return "aaa/a1";
+	}
+	
+}
